128cfb27e4a6c5f9f1aa55d587b709ba720dd7e1
Lucrumae <lucrumaeternus@gmail.com>
2026-03-01 17:12:56 +0700

    Delete reboot-daemon.sh

--- reboot-daemon.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Poll /tmp/ft_reboot_now setiap detik, reboot jika ada
-rm -f /tmp/ft_reboot_now
-while true; do
-    [ -f /tmp/ft_reboot_now ] && rm -f /tmp/ft_reboot_now && sleep 1 && reboot && exit
-    sleep 1
-done
